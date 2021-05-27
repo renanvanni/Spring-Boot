3e900b9139c5608b3faf6b17d5d457ce17087e8b
Renan Gonçalves Vanni <renanvanni13@gmail.com>
2021-05-27 10:51:32 -0300

    Alteração da pasta application.properties e PostagemController

--- blogPessoal/src/main/java/org/generation/blogpessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogpessoal/controller/PostagemController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.generation.blogpessoal.model.Postagem;
 import org.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,8 +37,23 @@ public class PostagemController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/title/{title}")
+	@GetMapping("/title/{title}")//procura a palavra pelo titulo e depois da / vc escreve o que quer encontrar
 	public ResponseEntity<List<Postagem>> GetByTitle(@PathVariable String title){
-		return ResponseEntity.ok(repository.findAllByTitleContainingIgnoreCase(title));
+		return ResponseEntity.ok(repository.findAllByTitleContainingIgnoreCase(title));//esse item findAllByTitleContainingIgnoreCase foi criado na pasta PostagemRepository
+	}
+	
+	@PostMapping//serve para postar algo no postman, por exemplo(e não pode passar o id no postman)
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));//utiliza o created para criar uma nova postagem e o body serve para dentro do corpo da mensagem ser salvo a nova postagem
+	}
+	
+	@PutMapping//serve para atualizar algum dado de uma postagem anterior(e tem que passar o id no postman)
+	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));//utiliza o OK para encontrar na lista o id que queremos alterar e o body atualiza ela
+	}
+	
+	@DeleteMapping("/{id}")//deleta através do id do item, por isso no postman tem que colocar /id que vc quer deletar
+	public void deletePostagem(@PathVariable long id) {//por causa do void que não precisa de return na próxima linha e tem que ser long, pq na model id foi definido como long
+		repository.deleteById(id);
 	}
 }
